b1463441457d9df62181cab860f65f99700ecaf1
nagu <hemanagu2005@gmail.com>
2026-06-01 14:49:09 +0530

    final

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import json
 
-be_url = "http://localhost:8000"
+be_url = st.secrets["be_url_server"]
 st.title("AI Interview Helper Bot")
 
 with st.form("Details"):
